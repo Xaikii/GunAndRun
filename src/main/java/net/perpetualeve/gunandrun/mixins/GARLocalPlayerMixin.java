afa6a6f63a4551f1e69ddc72cd8a401c6c8bf846
Xaikii <xaikii@outlook.de>
2023-02-07 19:27:29 +0100

    clean

--- src/main/java/net/perpetualeve/gunandrun/mixins/GARLocalPlayerMixin.java
+++ src/main/java/net/perpetualeve/gunandrun/mixins/GARLocalPlayerMixin.java
@@ -2,7 +2,6 @@ package net.perpetualeve.gunandrun.mixins;
 
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
